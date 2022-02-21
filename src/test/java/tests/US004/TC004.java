70a0630293432d323105b1963574f732a3f438de
Fadime ILHAN <fadimevurmazilhan@gmail.com>
2022-02-21 04:01:27 -0800

    v

--- src/test/java/tests/US004/TC004.java
+++ src/test/java/tests/US004/TC004.java
@@ -19,7 +19,7 @@ public class TC004  extends TestBaseRapor {
     HotelListPage hotelListPage = new HotelListPage();
 
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
         extentTest=extentReports.createTest("TC004","ilgili alanlar dolduruldu");
         //1-https://www.hotelmycamp.com/ Url ile giris yapılır
         Driver.getDriver().get(ConfigReader.getProperty("HMCURL"));
@@ -45,9 +45,7 @@ public class TC004  extends TestBaseRapor {
         //10)sol taraftaki Add Hotel butonuna tikla
         hotelListPage.ADDHOTELButonu.click();
 
-        //11)Create hotel yazisi goruldugunu test et
-        hotelListPage.CreateHotelButonu.click();
-        Assert.assertTrue(hotelListPage.CreateHotelButonu.isDisplayed());
+
 
       //  11-ilgili alanlari  giriniz
         Actions action=new Actions(Driver.getDriver());
@@ -63,9 +61,21 @@ public class TC004  extends TestBaseRapor {
                 .sendKeys("dddd@gmail").perform();
         // 16-IDGroup dan select group a tiklayiniz
         //  17-select group
-    extentTest.info("telefon ve email e farkli karakter girildigi zaman  bile test pass yapiyor yapmamasi lazim ");
+
         Select select=new Select(hotelListPage.CreateHotelIDGroupButonu);
         select.selectByIndex(2);
+  extentTest.info("bilgiler girildi");
+        //12-save butonuna tiklayin
+        hotelListPage.CreateHotelSaveButonu.click();
+        // ReusableMethods.getScreenShots("UsernamesS","target/screenShot/US004");
+  extentTest.info("save butonuna tiklandi");
+        Thread.sleep(5000);
+        // 13-save isleminin sonunda "Hotel was inserted successfully" yazisinin  goruldugunu test et
+
+        Assert.assertTrue(hotelListPage.MesajKutusuButonu.isDisplayed());
+  extentTest.info("Hotel was inserted successfully yazisi goruldu");
+        hotelListPage.MesajOkButonu.click();
+        extentTest.info("ok butonuna tiklanildi");
 
 
         Driver.closeDriver();
